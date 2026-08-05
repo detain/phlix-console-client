4909428e7261efc8648de1cd18c416b93dd46035
Joe Huss <detain@interserver.net>
2026-08-05 19:12:55 -0400

    feat(hub): add InviteLinkCreatedMsg and InviteLinkRevokedMsg

---
+++ src/Msg/InviteLinkCreatedMsg.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * @copyright 2026 Joe Huss <detain@interserver.net>
+ * @license   MIT
+ */
+
+namespace Phlix\Console\Msg;
+
+use SugarCraft\Core\Msg;
+
+final readonly class InviteLinkCreatedMsg implements Msg
+{
+    /**
+     * @param array{id:string,code:string,url:string} $link
+     */
+    public function __construct(
+        public array $link,
+    ) {}
+}
---
+++ src/Msg/InviteLinkRevokedMsg.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * @copyright 2026 Joe Huss <detain@interserver.net>
+ * @license   MIT
+ */
+
+namespace Phlix\Console\Msg;
+
+use SugarCraft\Core\Msg;
+
+final readonly class InviteLinkRevokedMsg implements Msg
+{
+}
