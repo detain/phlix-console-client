c157faf22ec337ffe39d116599dcb2d959b694be
Joe Huss <detain@interserver.net>
2026-08-05 19:12:33 -0400

    feat(hub): add InviteLinks Msg classes

---
+++ src/Msg/InviteLinksLoadedMsg.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * @copyright 2026 Joe Huss <detain@interserver.net>
+ * @license   MIT
+ */
+
+namespace Phlix\Console\Msg;
+
+use SugarCraft\Core\Msg;
+
+/** The invite links list arrived. */
+final readonly class InviteLinksLoadedMsg implements Msg
+{
+    /**
+     * @param list<array{id:string,code:string,label:string,uses:int,max_uses:int|null,expires_at:string|null}> $links
+     */
+    public function __construct(
+        public array $links,
+    ) {}
+}
